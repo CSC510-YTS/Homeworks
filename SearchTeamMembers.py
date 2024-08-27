c913fcc105685c9ea2a4ee6709f0ea47c3fd7318
Yunfei Chen <125923497+ychen-207523@users.noreply.github.com>
2024-08-27 19:19:49 +0000

    move the content of SearchTeamMembers to my/home/codespace/.python/current/bin/python3 /workspaces/HW1/myfile.py

--- SearchTeamMembers.py
+++
@@ -1,8 +0,0 @@
-from myfile import team_member
-
-print(team_member("Tanuj"))
-print(team_member("Shubham"))
-print(team_member("Yunfei"))
-print(team_member("Dijkstra"))
-
-# team_members("WRONG METHOD NAME!")
