eb032cde2c12a918523b7edee0d260c319c37ce0
Yunfei Chen <125923497+ychen-207523@users.noreply.github.com>
2024-09-04 19:26:02 +0000

    [HW2] | [Yunfei] | update pylint change

--- HW2/rand.py
+++ HW2/rand.py
@@ -1,10 +1,16 @@
+"""
+This file is to grenarte a random array
+"""
 import subprocess
 
 
 def random_array(arr):
+    """
+    This function is to grenarte a random array 
+    """
     shuffled_num = None
-    for i in range(len(arr)):
+    for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
     return arr
