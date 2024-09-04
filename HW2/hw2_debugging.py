e67832d3745fafb4a58481fbdfe02d114ed2d630
tanuj12 <43450384+tanuj12@users.noreply.github.com>
2024-09-04 20:51:58 +0000

    [HW2] | <Tanuj> | Fixed failing code and refacoted to have less time complexity

--- HW2/hw2_debugging.py
+++ HW2/hw2_debugging.py
@@ -27,20 +27,29 @@ def recombine(left_arr, right_arr):
     merge_arr = [0] * (len(left_arr) + len(right_arr))
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
             merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
             left_index += 1
+        else:
             merge_arr[left_index + right_index] = right_arr[right_index]
-
-    merge_sub_array(left_index, right_index, right_arr, merge_arr)
-    merge_sub_array(right_index, left_index, left_arr, merge_arr)
+            right_index += 1
+        merge_sub_array(left_index, right_index, right_arr, left_arr, merge_arr)
 
     return merge_arr
 
-def merge_sub_array(start_index: int, sub_start_index: int, sub_arr: list[int], merge_arr: list[int]):
+
+def merge_sub_array(
+        left_index: int,
+        right_index: int,
+        right_arr: list[int],
+        left_arr: list[int],
+        merge_arr: list[int]):
+    sub_arr = right_arr if right_index < len(right_arr) else left_arr
+    sub_start_index = right_index if right_index < len(right_arr) else left_index
+    start_index = left_index if right_index < len(right_arr) else right_index
+    
     for i in range(sub_start_index, len(sub_arr)):
-        merge_arr[start_index + sub_start_index] = sub_arr[i]
+        merge_arr[start_index + i] = sub_arr[i]
+
 
 target_arr = rand.random_array([None] * 20)
 arr_out = merge_sort(target_arr)
--- HW2/merge_sort_test.py
+++ HW2/merge_sort_test.py
@@ -1,10 +1,19 @@
 import pytest
 import copy
+import rand
 from hw2_debugging import merge_sort
+
 
 def test_merge_sort_ascending_ordered_array():
     ascending_sorted_array = [x for x in range(1, 50)]
     ascending_sorted_array_deep_copy = copy.deepcopy(ascending_sorted_array)
-    merge_sort(ascending_sorted_array_deep_copy)
+    sorted_array = merge_sort(ascending_sorted_array_deep_copy)
 
-    assert ascending_sorted_array == ascending_sorted_array_deep_copy
+    assert sorted_array == ascending_sorted_array_deep_copy
+
+
+def test_merge_sort_radom__unsorted_array():
+    random_unsorted_array = [2, 1, 25, 89, -1, 6, 8, 4]
+    sorted_array = merge_sort(random_unsorted_array)
+
+    assert sorted_array == [-1, 1, 2, 4, 6, 8, 25, 89]
