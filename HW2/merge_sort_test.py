fc3f82f12588139b2df2bb3cca7060d7872dde7f
ShubhamTidke <42945527+ShubhamTidke@users.noreply.github.com>
2024-09-04 20:06:30 +0000

    Rename test_merge_sort_ascending_ordered_array method

--- HW2/merge_sort_test.py
+++ HW2/merge_sort_test.py
@@ -2,7 +2,7 @@ import pytest
 import copy
 from hw2_debugging import merge_sort
 
-def test_merge_sort():
+def test_merge_sort_ascending_ordered_array():
     ascending_sorted_array = [x for x in range(1, 50)]
     ascending_sorted_array_deep_copy = copy.deepcopy(ascending_sorted_array)
     merge_sort(ascending_sorted_array_deep_copy)
