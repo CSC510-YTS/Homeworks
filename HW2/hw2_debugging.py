7b0829aeeb61232b54055124ab94b11d6fdd6318
ShubhamTidke <42945527+ShubhamTidke@users.noreply.github.com>
2024-09-04 20:05:01 +0000

    Fixed method complexity as per Radon analysis tool

--- HW2/hw2_debugging.py
+++ HW2/hw2_debugging.py
@@ -24,7 +24,7 @@ def recombine(left_arr, right_arr):
     """
     left_index = 0
     right_index = 0
-    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    merge_arr = [0] * (len(left_arr) + len(right_arr))
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
             right_index += 1
@@ -33,14 +33,14 @@ def recombine(left_arr, right_arr):
             left_index += 1
             merge_arr[left_index + right_index] = right_arr[right_index]
 
-    for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
-
-    for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+    merge_sub_array(left_index, right_index, right_arr, merge_arr)
+    merge_sub_array(right_index, left_index, left_arr, merge_arr)
 
     return merge_arr
 
+def merge_sub_array(start_index: int, sub_start_index: int, sub_arr: list[int], merge_arr: list[int]):
+    for i in range(sub_start_index, len(sub_arr)):
+        merge_arr[start_index + sub_start_index] = sub_arr[i]
 
 target_arr = rand.random_array([None] * 20)
 arr_out = merge_sort(target_arr)
