724bbb424fb65bbfe25497e05c64445e1ef4484b
Yunfei Chen <125923497+ychen-207523@users.noreply.github.com>
2024-09-05 19:40:21 +0000

    [HW2] | <Yunfei> | Added descending ordered test

--- HW2/merge_sort_test.py
+++ HW2/merge_sort_test.py
@@ -11,8 +11,15 @@ def test_merge_sort_ascending_ordered_array():
     assert sorted_array == ascending_sorted_array_deep_copy
 
 
-def test_merge_sort_radom__unsorted_array():
+def test_merge_sort_random__unsorted_array():
     random_unsorted_array = [2, 1, 25, 89, -1, 6, 8, 4]
     sorted_array = merge_sort(random_unsorted_array)
 
     assert sorted_array == [-1, 1, 2, 4, 6, 8, 25, 89]
+
+
+def test_merge_sort_descending_ordered_array():
+    descending_sorted_array = [x for x in range(50, 0, -1)]
+    sorted_array = merge_sort(descending_sorted_array)
+
+    assert sorted_array == sorted(descending_sorted_array)
