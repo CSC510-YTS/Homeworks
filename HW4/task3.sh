69e6b850a6980bef9c641232e061692b8f51e11d
Yunfei <ychen207523@gmail.com>
2024-10-05 13:58:39 -0400

    <HW4> | pushed task3.sh

---
+++ HW4/task3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gawk -F, '$3 == 2 && $NF ~ /S/' titanic.csv | sed 's/female/F/g' | sed 's/male/M/g' | gawk -F, '{
+    if($(NF-6) != null ) {
+        sum += $(NF-6);
+        count++;
+    }
+    print $0;
+} END {
+    print "Average age is " sum / count;
+}'
