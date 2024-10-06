d0be7c451c04275113db972c14a31d82766b26cd
Yunfei <ychen207523@gmail.com>
2024-10-06 13:24:40 -0400

    <HW4> | update task3 path for titanic.csv

--- HW4/task3.sh
+++ HW4/task3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gawk -F, '$3 == 2 && $NF ~ /S/' hw4/titanic.csv | sed 's/female/F/g' | sed 's/male/M/g' | gawk -F, '{
+gawk -F, '$3 == 2 && $NF ~ /S/' titanic.csv | sed 's/female/F/g' | sed 's/male/M/g' | gawk -F, '{
     if($(NF-6) != null ) {
         sum += $(NF-6);
         count++;
