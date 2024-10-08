c370d47f0401fa38f25b5fa96823cc64788a711c
ShubhamTidke <42945527+ShubhamTidke@users.noreply.github.com>
2024-10-07 22:44:25 -0400

    Update path for task2.sh

--- HW4/task2.sh
+++ HW4/task2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -r -l 'sample' hw4/dataset1/ | xargs grep -c 'CSC510' | grep -P -v ':[012]$' | gawk -F: '{ "ls -l "$1"" |& getline fileDetails; print fileDetails, $2}' | sort -k10,10nr -k5,5nr | cut -d ' ' -f10 | sed 's\'hw4/dataset1/file_'\filtered_\'
+grep -r -l 'sample' dataset1/ | xargs grep -c 'CSC510' | grep -P -v ':[012]$' | gawk -F: '{ "ls -l "$1"" |& getline fileDetails; print fileDetails, $2}' | sort -k10,10nr -k5,5nr | cut -d ' ' -f10 | sed 's\'hw4/dataset1/file_'\filtered_\'
